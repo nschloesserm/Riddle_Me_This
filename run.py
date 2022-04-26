c6898303e3a08ea332fda6c7b21cfaa89adc5854
nschloesserm <nicholasmckennaart@gmail.com>
2022-04-26 15:40:51 +0000

    add random riddlegame function

--- run.py
+++ run.py
@@ -33,7 +33,35 @@ def riddleGameStart():
 import random
 
 def riddleGame():
-       
+    for riddles_needed in range(int("3")):
+        riddle_one = 1
+        riddle_two = 2
+        riddle_three = 3
+
+        riddle_number = random.randint(1, 3)
+
+        if riddle_number == 1:
+            print("\nriddleone?\n")
+            riddle_one_answer = input()
+            if riddle_one_answer == "a":
+                print("\ncorrect!\n")
+            else:
+                print("\nworng!\n")
+        if riddle_number == 2:
+            print("\nriddletwo?\n")
+            riddle_two_answer = input()
+            if riddle_two_answer == "b":
+                print("\ncorrect!\n")
+            else:
+                print("\nworng!\n")
+        if riddle_number == 3:
+            print("\nriddlethree?\n")
+            riddle_three_answer = input()
+            if riddle_three_answer == "c":
+                print("\ncorrect!\n")
+            else:
+                print("\nworng!\n")
+    
 
 riddleGameStart()
-riddleGame()
+
